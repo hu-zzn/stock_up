740126529cf916d1d3435db0f1408c7d557d59d2
Hussain Abbas Zaidi <hussainabbaszaidi7@gmail.com>
2025-10-26 06:21:54 +0530

    home page 1

---
+++ lib/pages/home.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appbar(),
+      body: body()
+    );
+  }
+
+  AppBar appbar() {
+    return AppBar(
+      title: Text('StockUp', style: TextStyle(fontWeight: FontWeight.bold)),
+      centerTitle: true,
+      backgroundColor: const Color.fromARGB(255, 239, 220, 220),
+      elevation: 0.0,
+      //left button
+      leading: GestureDetector(
+        onTap: () {},
+        child: Container(
+          margin: EdgeInsets.all(10),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 239, 230, 240),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: SvgPicture.asset(
+            'assets/icons/left_arrow.svg',
+            height: 20,
+            width: 20,
+          ),
+        ),
+      ),
+      actions: [
+        GestureDetector(
+          onTap: () {},
+          child: Container(
+            margin: EdgeInsets.all(10),
+            alignment: Alignment.center,
+            width: 40,
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 239, 230, 240),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: SvgPicture.asset(
+              'assets/icons/dots.svg',
+              height: 20,
+              width: 20,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+
+
+  Column body() {
+    return Column(
+      children: [
+        Container(
+          margin: EdgeInsets.only(top:40,left:20,right:20),
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: const Color.fromARGB(205, 189, 189, 189),        // shadow color
+                blurRadius: 10,            // soften the shadow
+                spreadRadius: 2,           // extend the shadow
+                offset: Offset(0, 5),      // move it slightly down
+              ),
+            ],
+          ),
+          child: TextField(
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              
+              border: OutlineInputBorder(
+                borderSide: BorderSide.none,
+                borderRadius: BorderRadius.circular(10)
+              )
+            ),
+          )
+        )
+      ]
+    );
+  }
+}
